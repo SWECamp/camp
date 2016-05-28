3810d913aa924d4fe17942ecc7479f75a26d29f0
protal <slipknot.opal@gmail.com>
2016-05-28 12:39:45 +0700

    edit form

--- application/views/register/content.php
+++ application/views/register/content.php
@@ -1,7 +1,46 @@
 <section class="choose-theme section-padding color-white"  id="about">
         <div class="container">
         <h1>สมัครเข้าร่วมการประชุม</h1>
-                        
+        <div class="col-md-2">
+        	
+        </div>
+		<div class="col-md-8 text-center">
+			<div class="form-group col-lg-12">
+					<label>Username</label>
+					<input type="" name="" class="form-control" id="" value="">
+				</div>
+				
+				<div class="form-group col-lg-6">
+					<label>Password</label>
+					<input type="password" name="" class="form-control" id="" value="">
+				</div>
+				
+				<div class="form-group col-lg-6">
+					<label>Repeat Password</label>
+					<input type="password" name="" class="form-control" id="" value="">
+				</div>
+								
+				<div class="form-group col-lg-6">
+					<label>Email Address</label>
+					<input type="" name="" class="form-control" id="" value="">
+				</div>
+				
+				<div class="form-group col-lg-6">
+					<label>Repeat Email Address</label>
+					<input type="" name="" class="form-control" id="" value="">
+				</div>			
+				
+				<div class="col-sm-6">
+					<input type="checkbox" class="checkbox" />Sigh up for our newsletter
+				</div>
+
+				<div class="col-sm-6">
+					<input type="checkbox" class="checkbox" />Send notifications to this email
+				</div>			
+		</div>
+		<div class="col-md-2">
+        	
+        </div>
             
         </div>
     </section>
