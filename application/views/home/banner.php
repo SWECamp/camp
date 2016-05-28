c0af02531a879a884717f387d1c2f8d9618713be
protal <slipknot.opal@gmail.com>
2016-05-28 11:11:14 +0700

    edit banner

--- application/views/home/banner.php
+++ application/views/home/banner.php
@@ -2,15 +2,31 @@
         <div class="texture">
         <div class="container">
            <div class="col-5 mb-12 mb-center banner-box">
-               
-               <div class="banner_line1">The great aim of education is not knowledge but action.</div>
-               <div class="banner_brief_text">Education not only broadens the mind but adds the most important component for critical thought, depth.</div>
+               <div class="banner_line1">การประชุมวิชาการระดับประเทศ</div>
+               <div class="banner_brief_text">มหาวิทยาลัยวลัยลักษณ์ได้รับเกียรติให้เป็นเจ้าภาพการประชุมวิชาการระดับประเทศ NCIT 2560 โดยมีเป้าหมายเพื่อกระตุ้นและส่งเสริมการวิจัยและพัฒนาองค์ความรู้ทางด้านเทคโนโลยีสารสนเทศภายในประเทศให้แพร่หลาย </div>
                <div    class="mb-center mb-12">
-               <div class="button-1">
+               <div class="button-1 text-center">
                    <a href="<?php echo site_url('camp/login') ?>">เข้าสู่ระบบ</a></div></div>          
-           </div>
-            <div class="col-7 mb-12 banner-image"  style="position:relative;"><!--<img src="image/banner2_image.png"  />--></div>
-        </div>
+          </div>
+          <div class="col-7 mb-12 banner-box" style=" text-align: left;    padding-left: 20px;"  >
+             <div class="banner_line1"> กำหนดการ</div>
+              <div class="banner_brief_text">
+                <ul>
+                  <li>สมัครส่งบทความวิจัย :
+1 พฤษภาคม – 17 สิงหาคม พ.ศ. 2560</li>
+                  <li>แจ้งผลการคัดเลือกบทความวิจัย :
+19 กันยายน พ.ศ. 2560</li>
+                  <li>ส่งบทความพร้อมตีพิมพ์ :
+7 ตุลาคม พ.ศ. 2560</li>
+                  <li>วันสุดท้ายของการลงทะเบียน :
+14 ตุลาคม พ.ศ. 2560</li>
+                  <li>การนำเสนอผลงาน :
+25-26 ตุลาคม พ.ศ. 2560</li>
+                </ul>
+              </div>
+          </div>
+             
+          </div>
             </div>
        <!--  <div class="col-12 banner-extra-div color-black-s-light">
  
