0f4dae4f56cf3caac3597879f69b58bae552bfc8
protal <slipknot.opal@gmail.com>
2016-05-28 13:20:28 +0700

    edit form register

--- application/views/register/content.php
+++ application/views/register/content.php
@@ -4,39 +4,230 @@
         <div class="col-md-2">
         	
         </div>
-		<div class="col-md-8 text-center">
-			<div class="form-group col-lg-12">
-					<label>Username</label>
-					<input type="" name="" class="form-control" id="" value="">
+		<div class="col-md-8 text-left">
+			<form>
+				<div class="form-group col-lg-2">
+					<label>คำนำหน้า</label>
+					<select name="" form="">
+					  <option value="">นาย</option>
+					  <option value="">นาง</option>
+					  <option value="">นางสาว</option>
+
+					</select>
 				</div>
-				
-				<div class="form-group col-lg-6">
-					<label>Password</label>
-					<input type="password" name="" class="form-control" id="" value="">
+				<div class="form-group col-lg-5">
+					<label>ชื่อ</label>
+					<input type="text" name="" class="form-control" id="" value="" placeholder="ชื่อ" > 
 				</div>
-				
-				<div class="form-group col-lg-6">
-					<label>Repeat Password</label>
-					<input type="password" name="" class="form-control" id="" value="">
-				</div>
-								
-				<div class="form-group col-lg-6">
-					<label>Email Address</label>
-					<input type="" name="" class="form-control" id="" value="">
-				</div>
-				
-				<div class="form-group col-lg-6">
-					<label>Repeat Email Address</label>
-					<input type="" name="" class="form-control" id="" value="">
-				</div>			
-				
-				<div class="col-sm-6">
-					<input type="checkbox" class="checkbox" />Sigh up for our newsletter
+				<div class="form-group col-lg-5">
+					<label>นามสกุล</label>
+					<input type="text" name="" class="form-control" id="" value="" placeholder="นามสกุล"> 
 				</div>
 
-				<div class="col-sm-6">
-					<input type="checkbox" class="checkbox" />Send notifications to this email
-				</div>			
+
+				<div class="form-group col-lg-12">
+					<label>ที่อยู่</label><br>
+					<textarea style="height:100px;width:100%;" rows="6" cols="50" placeholder="181/52 ...."></textarea>
+ 
+				</div>
+				<div class="form-group col-lg-6">
+					<label>จังหวัด</label><br>
+					<select name="" form="" style="width:100%;" >
+					  <option value="0">- เลือกจังหวัด -</option>
+
+
+<option value="64">กระบี่   </option>
+
+<option value="1">กรุงเทพมหานคร   </option>
+
+<option value="56">กาญจนบุรี   </option>
+
+<option value="34">กาฬสินธุ์   </option>
+
+<option value="49">กำแพงเพชร   </option>
+
+<option value="28">ขอนแก่น   </option>
+
+<option value="13">จันทบุรี   </option>
+
+<option value="15">ฉะเชิงเทรา   </option>
+
+<option value="11">ชลบุรี   </option>
+
+<option value="9">ชัยนาท   </option>
+
+<option value="25">ชัยภูมิ   </option>
+
+<option value="69">ชุมพร   </option>
+
+<option value="72">ตรัง   </option>
+
+<option value="14">ตราด   </option>
+
+<option value="50">ตาก   </option>
+
+<option value="17">นครนายก   </option>
+
+<option value="58">นครปฐม   </option>
+
+<option value="36">นครพนม   </option>
+
+<option value="19">นครราชสีมา   </option>
+
+<option value="63">นครศรีธรรมราช   </option>
+
+<option value="47">นครสวรรค์   </option>
+
+<option value="3">นนทบุรี   </option>
+
+<option value="76">นราธิวาส   </option>
+
+<option value="43">น่าน   </option>
+
+<option value="77">บึงกาฬ</option>
+
+<option value="20">บุรีรัมย์   </option>
+
+<option value="4">ปทุมธานี   </option>
+
+<option value="62">ประจวบคีรีขันธ์   </option>
+
+<option value="16">ปราจีนบุรี   </option>
+
+<option value="74">ปัตตานี   </option>
+
+<option value="5">พระนครศรีอยุธยา   </option>
+
+<option value="44">พะเยา   </option>
+
+<option value="65">พังงา   </option>
+
+<option value="73">พัทลุง   </option>
+
+<option value="53">พิจิตร   </option>
+
+<option value="52">พิษณุโลก   </option>
+
+<option value="66">ภูเก็ต   </option>
+
+<option value="32">มหาสารคาม   </option>
+
+<option value="37">มุกดาหาร   </option>
+
+<option value="75">ยะลา   </option>
+
+<option value="24">ยโสธร   </option>
+
+<option value="33">ร้อยเอ็ด   </option>
+
+<option value="68">ระนอง   </option>
+
+<option value="12">ระยอง   </option>
+
+<option value="55">ราชบุรี   </option>
+
+<option value="7">ลพบุรี   </option>
+
+<option value="40">ลำปาง   </option>
+
+<option value="39">ลำพูน   </option>
+
+<option value="22">ศรีสะเกษ   </option>
+
+<option value="35">สกลนคร   </option>
+
+<option value="70">สงขลา   </option>
+
+<option value="71">สตูล   </option>
+
+<option value="2">สมุทรปราการ   </option>
+
+<option value="60">สมุทรสงคราม   </option>
+
+<option value="59">สมุทรสาคร   </option>
+
+<option value="10">สระบุรี</option>
+
+<option value="18">สระแก้ว   </option>
+
+<option value="8">สิงห์บุรี   </option>
+
+<option value="57">สุพรรณบุรี   </option>
+
+<option value="67">สุราษฎร์ธานี   </option>
+
+<option value="21">สุรินทร์   </option>
+
+<option value="51">สุโขทัย   </option>
+
+<option value="31">หนองคาย   </option>
+
+<option value="27">หนองบัวลำภู   </option>
+
+<option value="6">อ่างทอง   </option>
+
+<option value="26">อำนาจเจริญ   </option>
+
+<option value="29">อุดรธานี   </option>
+
+<option value="41">อุตรดิตถ์   </option>
+
+<option value="48">อุทัยธานี   </option>
+
+<option value="23">อุบลราชธานี   </option>
+
+<option value="45">เชียงราย   </option>
+
+<option value="38">เชียงใหม่   </option>
+
+<option value="61">เพชรบุรี   </option>
+
+<option value="54">เพชรบูรณ์   </option>
+
+<option value="30">เลย   </option>
+
+<option value="42">แพร่   </option>
+
+<option value="46">แม่ฮ่องสอน   </option>
+					</select>
+				</div>
+				<div class="form-group col-lg-6">
+					<label>รหัสไปรษณี</label>
+					<input type="text" name="" class="form-control" id="" value="" placeholder="รหัสไปรษณี" > 
+
+				</div>
+				<div class="form-group col-lg-6">
+					<label>เบอร์โทรศัพท์</label>
+					<input type="text" name="" class="form-control" id="" value="" placeholder="เบอร์โทรศัพท์" > 
+				</div>
+				<div class="form-group col-lg-6">
+					<label>e-mail</label>
+					<input type="email" name="" class="form-control" id="" value="" placeholder="e-mail" > 
+				</div>
+				<div class="form-group col-lg-12">
+					<label>หน่วยงานต้นสังกัด</label>
+					<input type="text" name="" class="form-control" id="" value="" placeholder="" > 
+				</div>
+				<div class="form-group col-lg-12">
+					<label>รายการแพ้อาหาร</label><br>
+					<textarea style="height:100px;width:100%;" rows="6" cols="50"></textarea>
+ 
+				</div>
+				<hr>
+				<div class="form-group col-lg-12">
+					<label>รหัสผ่าน</label>
+					<input type="password" name="" class="form-control" id="" value="" min="6" placeholder="กรุณาใส่รหัสผ่านอย่างน้อย 6 ตัวอักษร" > 
+				</div>
+				<div class="form-group col-lg-12">
+					<label>ใส่รหัสผ่านอีกครั้ง</label>
+					<input type="password" name="" class="form-control" id="" value="" min="6" placeholder="กรุณายื่นยันรหัสผ่าน" > 
+				</div>
+				<div class="form-group col-lg-12 text-center	">
+					<button type="button" class="btn btn-primary">ยืนยัน</button>
+				</div>
+
+				
+		</form>
 		</div>
 		<div class="col-md-2">
         	
