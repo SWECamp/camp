b64d8cffc9917ccb6a18c7d4033d2cbcf8aa10b8
protal <slipknot.opal@gmail.com>
2016-05-28 19:21:28 +0700

    แก้ไข user

--- application/views/user/content.php
+++ application/views/user/content.php
@@ -5,20 +5,21 @@
 
         <?php 
         foreach ($result->result() as $value) {
-        	echo "ชื่อ-สกุล:".$value->firstname." ";
-        	echo $value->lastname."<br>";
-        	echo "ที่อยู่ :".$value->address."<br>";
-        	//echo $value->province."<br>";
-        	echo "รหัสไปรษณีย์ :".$value->postalcode."<br>";
-        	echo "เบอร์โทร: ".$value->phoneno."<br>";
-        	echo "e-mail: ".$value->email."<br>";
-        	echo "หน่วยงานต้นสังกัด: ".$value->department."<br>";
-        	echo "รายการแพ้อาหาร: ".$value->food."<br>";
-        	//echo $value->artifact."<br>";
-        	//echo $value->join."<br>";
-        	//echo $value->tour1."<br>";
-        	//echo $value->tour2."<br>";
-        	//echo $value->tour3."<br>";
+        	$user = $value;
+        	// echo "ชื่อ-สกุล:".$value->firstname." ";
+        	// echo $value->lastname."<br>";
+        	// echo "ที่อยู่ :".$value->address."<br>";
+        	// //echo $value->province."<br>";
+        	// echo "รหัสไปรษณีย์ :".$value->postalcode."<br>";
+        	// echo "เบอร์โทร: ".$value->phoneno."<br>";
+        	// echo "e-mail: ".$value->email."<br>";
+        	// echo "หน่วยงานต้นสังกัด: ".$value->department."<br>";
+        	// echo "รายการแพ้อาหาร: ".$value->food."<br>";
+        	// //echo $value->artifact."<br>";
+        	// //echo $value->join."<br>";
+        	// //echo $value->tour1."<br>";
+        	// //echo $value->tour2."<br>";
+        	// //echo $value->tour3."<br>";
         	
         	       }
          ?>     
