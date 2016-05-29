a025aac12f9c4d042bbea0569c26c80cf68f57a1
protal <slipknot.opal@gmail.com>
2016-05-29 11:11:34 +0700

    แก้ไขหน้า UI

--- application/views/admin/content.php
+++ application/views/admin/content.php
@@ -1,26 +1,31 @@
 <section class="choose-theme section-padding color-white"  id="about">
         <div class="container">
         <h1>ADMIN</h1>
-                        
-            <center><table borber="1">
-            	<tr>
+
+
+        <table class="table table-hover">
+            <thead>
+                <tr>
                     <th style="text-align:center">ชื่อต้น</th>
-            		<th style="text-align:center">ชื่อ</th>
+                    <th style="text-align:center">ชื่อ</th>
                     <th style="text-align:center">นามสกุล</th>
                     <th style="text-align:center">ประเภท</th>
                     <th style="text-align:center">จำนวนเงิน</th>
                     <th style="text-align:center">วันที่ชำระเงิน</th>
-            		<th style="text-align:center">สถานะการชำระเงิน</th>
-            	</tr>
-        <?php 
+                    <th style="text-align:center">สถานะการชำระเงิน</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <?php 
         foreach($data->result() as $row) { ?>
         <tr>
                     <td><?php if($row->title == 1){ echo "นาย";
                     }else if($row->title == 1){ echo "นาง";
                         }else{ echo "นางสาว";
                         } ?></td>
-                    <td><?php echo $row->firstname; ?></td>
-                    <td><?php echo $row->lastname; ?></td>
+                    <td class="text-left"><?php echo $row->firstname; ?></td>
+                    <td class="text-left"><?php echo $row->lastname; ?></td>
                     <td><?php if($row->join == 1) echo "ผู้นำเสนอผลงาน";
                     else echo "ผู้สนใจเข้าร่วมงาน"  ?></td>
                     <td></td>
@@ -29,13 +34,18 @@
                         if($row->artifact == 1){
                             echo "<a href='".site_url('camp/reArtifact/'.$row->accountID)."'>ยืนยัน</a>";
                         }else{
-                            echo "ยืนยันการชำระเงินแล้ว";
+                            echo "<font style='color:green;'>ยืนยันการชำระเงินแล้ว</font>";
                         }
                      ?></td>
                     <!-- echo "<a href='".site_url('camp/reArtifact/'.$row->accountID)."'>ยืนยัน</a>":echo ""; -->
         </tr>
         <?php }
          ?>
-        </table></center>
+
+                </tr>
+            </tbody>
+        </table>
+
+
         </div>
     </section>
