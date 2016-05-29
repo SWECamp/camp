f788e08c69e31a758f550c5e096876741cc1d142
protal <slipknot.opal@gmail.com>
2016-05-29 10:53:13 +0700

    แก้แผนที่เพิ่ต่าง ๆ

--- application/views/home/map.php
+++ application/views/home/map.php
@@ -1,25 +1,11 @@
+
+<section class="map color-light-white" id="contact" style="padding-top: 30px;">
+  <h2>ตึกนวัตกรรม 222 ต.ไทยบุรี อ.ท่าศาลา จ.นครศรีธรรมราช 80160</h2><br><BR>
+  <img src="<?php echo base_url(); ?>template/image/map1.jpg" style="margin-bottom:20px">
+
+</section>
+        
 <section class="map color-light-white" id="contact">
           <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4800.104726082135!2d99.89191031375597!3d8.639902157318012!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0xe5759a5467b2cafe!2z4Lit4Li44LiX4Lii4Liy4LiZ4Lin4Li04LiX4Lii4Liy4Lio4Liy4Liq4LiV4Lij4LmMIOC4oeC4q-C4suC4p-C4tOC4l-C4ouC4suC4peC4seC4ouC4p-C4peC4seC4ouC4peC4seC4geC4qeC4k-C5jA!5e0!3m2!1sth!2sus!4v1464464426066" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
     </section>
     
-    <section class="contact-address section-padding color-light-white">
-        <div class="container">
-        <div class="col-12">
-            <div class="contact-box col-4">
-                <div class="col-12 contact-icon"><i class="quickweb-icon-direction"></i></div> 
-                          <div class="col-12 contact-title">อาคารนวัตกรรม  <br />มหาวิทยาลัยวลัยลักษณ์</div>
-
-                      </div>
-                   <div class="contact-box col-4">
-                       <div class="col-12 contact-icon"><i class="quickweb-icon-phone"></i></div>
-                       <div class="col-12 contact-title">075672206-7</div>
-
-                   </div> 
-                <div class="contact-box col-4">
-                     <div class="col-12 contact-icon"><i class="quickweb-icon-mail-alt"></i></div>
-                      <div class="col-12 contact-title">ncit2560wu.ac.th</div>
-                </div>
-
-            
-    </div></div>
-            </section>
